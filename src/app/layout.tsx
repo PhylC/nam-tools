fd7c305c45d617e76e3f95fff889c1b4b8ac5839
Phyl C <phyl.sion@gmail.com>
2026-08-17 17:12:40 +0100

    Render Bing verification as meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,8 @@ export const metadata: Metadata = {
   authors: [{ name: SITE_NAME }],
   creator: SITE_NAME,
   publisher: SITE_NAME,
-  verification: {
-    other: {
-      "msvalidate.01": "B225F1EE3C9E672FBA4084187B9749AB",
-    },
+  other: {
+    "msvalidate.01": "B225F1EE3C9E672FBA4084187B9749AB",
   },
   manifest: "/manifest.webmanifest",
   icons: {
